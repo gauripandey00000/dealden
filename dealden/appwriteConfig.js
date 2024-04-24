38f93528fcf91ccd13de14bff90a88b38266ddc4
gauripandey00000 <167735115+gauripandey00000@users.noreply.github.com>
2024-04-24 12:17:16 +0530

    Add files via upload

---
+++ dealden/appwriteConfig.js
@@ -0,0 +1,7 @@
+
+const appwrite = new Appwrite.client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('6625ea7bb2d7a969748b');
+
---
+++ dealden/indexx.js
@@ -0,0 +1,42 @@
+// {/* <script src="https://unpkg.com/appwrite/dist/esm/appwrite.min.js"></script> */}
+// // import {  } from "module";
+// const account = new Appwrite.Account(appwrite);
+
+// // Signup function
+// document.getElementById('signup').addEventListener('submit', function(event) {
+//     event.preventDefault();
+    
+//     const name = document.getElementById('signupName').value;
+//     const email = document.getElementById('signupEmail').value;
+//     const password = document.getElementById('signupPassword').value;
+
+//     account.create('unique()', email, password, name)
+//         .then(response => {
+//             console.log('User created:', response);
+//             // Handle successful signup (e.g., redirect to dashboard)
+//         })
+//         .catch(error => {
+//             console.error('Error creating user:', error);
+//         });
+// });
+
+// // Login function
+// document.getElementById('login').addEventListener('submit', function(event) {
+//     event.preventDefault();
+    
+//     const email = document.getElementById('loginEmail').value;
+//     const password = document.getElementById('loginPassword').value;
+
+//     account.createSession(email, password)
+//         .then(response => {
+//             console.log('Logged in:', response);
+//             // Handle successful login (e.g., redirect to dashboard)
+//         })
+//         .catch(error => {
+//             console.error('Error logging in:', error);
+//         });
+// });
+
+
+
+
